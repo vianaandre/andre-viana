e2aa68b0c81d83c9776ef9c753dd89eb76913011
vianaandre <andredapperviana@gmail.com>
2023-11-14 14:44:18 -0300

    feat: add project rico jogos

--- src/common/data/portfolio.ts
+++ src/common/data/portfolio.ts
@@ -3,13 +3,19 @@ import { ProjectProps } from 'common/interfaces/ProjectProps';
 
 export const projects: ProjectProps[] = [
     {
-        title: '01/Tua Agenda',
+        title: '01/Rico Jogos',
+        description: 'Plataforma de jogos online que oferece experiência imersiva para entusiastas de jogos. Ela incluí, listagem de jogos por categoria, sistema de autenticação, fluxo de depósito e saque, e um formulário para contato.',
+        technologies: 'ReactJs, Sass, Redux e Axios',
+        link: 'https://ricojogos.com/',
+    },
+    {
+        title: '02/Tua Agenda',
         description: 'Aplcação web para agendamentos de serviços que inclui listagem das empresas, perfil da empresa, fluxo de autenticação, fluxo de agendamento com etapa de pagamento, listagem dos agendamento do cliente e formulário de anamnese.',
         technologies: 'ReactJs, NextJs, Typescript, styled-components e Radix',
         link: 'https://tua-agenda-dev.vercel.app/',
     },
     {
-        title: '02/SynPass',
+        title: '03/SynPass',
         description: 'Aplicação para venda de ingressos que inclui uma lista organizada por categoria, autenticação de usuários, opção de compra de um ou mais ingressos, além de uma lista de ingressos comprados ou reservados para referência futura.',
         technologies: 'ReactJs, NextJs, Typescript, styled-components e Bootstrap',
         link: 'https://synpass.com.br/',
@@ -19,11 +25,5 @@ export const projects: ProjectProps[] = [
         description: 'Landing Page atraente com diversos efeitos visuais para aumentar o engajamento dos usuários e tornar a experiência de navegação mais envolvente. Além disso, trabalhei meticulosamente em SEO, otimizando o conteúdo e a estrutura da página para garantir que ela apareça nos primeiros resultados dos motores de busca.',
         technologies: 'ReactJs, NextJs, Typescript e styled-components',
         link: 'https://play.eventfy.com.br/',
-    },
-    {
-        title: '04/SICOM',
-        description: 'Aplicação para gerenciar os Módulos de Balancetes Contábeis.',
-        technologies: 'ReactJs, DevExtreme UI, Axios, SASS',
-        link: 'https://sicom.vercel.app/',
     },
 ];
