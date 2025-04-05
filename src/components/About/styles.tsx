658099b3e7ad424c829325572249d6d5dfcf6ffc
Andre Viana <andredapperviana@gmail.com>
2025-04-05 20:55:03 -0300

    fix: adapter imagem height by text about

--- src/components/About/styles.tsx
+++ src/components/About/styles.tsx
@@ -68,7 +68,7 @@ export const ContainerAbout = styled.section`
                 padding-left: ${({ theme }) => theme.spacing(4)};
                 display: flex;
                 flex-direction: column;
-                height: 440px;
+                height: fit-content;
                 @media(max-width: ${({ theme }) => theme.breakpoints.desktop}) {
                     height: 521px;
                 }
